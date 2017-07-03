48b6fd95857d039ae5a39888b71f668435b38d91
katrinHamburg <katrin.mischok@gmail.com>
2017-07-03 19:47:40 +0200

    fix test

--- src/main/resources/static/js/filsumflickr.js
+++ src/main/resources/static/js/filsumflickr.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 );
 function getPicturesFromFlickr() {
 
-    var url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=e5084b29ab011cc87bfac3880ad61b61&photoset_id=72157682633987222&format=json&nojsoncallback=?";
+    var url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=e5084b29ab011cc87bfac3880ad61b61&photoset_id=72157682633987222&format=json&nojsoncallback=1";
     $.getJSON(url, function(data){
         $.each(data.photoset.photo, function(i,item){
             var src_small = "http://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +"_q.jpg";
